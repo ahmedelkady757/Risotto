a54ac4da27a6df42393500ecf07714656d9fe03c
Ahmed Elkady <ahmed.elkady757@gmail.com>
2026-03-20 14:56:58 +0200

    feat(home): add MealRepository interface and implementation

---
+++ app/src/main/java/com/example/risotto/data/repository/MealRepositoryImpl.java
@@ -0,0 +1,113 @@
+package com.example.risotto.data.repository;
+
+import com.example.risotto.core.utils.AppLogger;
+import com.example.risotto.data.datasource.remote.RemoteDataSource;
+import com.example.risotto.data.model.Category;
+import com.example.risotto.data.model.Meal;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+
+public class MealRepositoryImpl implements MealRepository {
+
+    private final RemoteDataSource remoteDataSource;
+
+    public MealRepositoryImpl(RemoteDataSource remoteDataSource) {
+        this.remoteDataSource = remoteDataSource;
+    }
+
+
+    @Override
+    public Single<Meal> getRandomMeal() {
+        AppLogger.d("MealRepository: getRandomMeal");
+        return remoteDataSource.getRandomMeal()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Single<List<Meal>> getLatestMeals() {
+        AppLogger.d("MealRepository: getLatestMeals");
+        return remoteDataSource.getLatestMeals()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Single<List<Category>> getCategories() {
+        AppLogger.d("MealRepository: getCategories");
+        return remoteDataSource.getCategories()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+
+    @Override
+    public Single<Meal> getMealById(String id) {
+        AppLogger.d("MealRepository: getMealById → " + id);
+        return remoteDataSource.getMealById(id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+
+    @Override
+    public Single<List<Meal>> searchMealsByName(String name) {
+        AppLogger.d("MealRepository: searchMealsByName → " + name);
+        return remoteDataSource.searchMealsByName(name)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Single<List<Meal>> searchMealsByFirstLetter(String letter) {
+        AppLogger.d("MealRepository: searchMealsByFirstLetter → " + letter);
+        return remoteDataSource.searchMealsByFirstLetter(letter)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Single<List<Meal>> filterByCategory(String category) {
+        AppLogger.d("MealRepository: filterByCategory → " + category);
+        return remoteDataSource.filterByCategory(category)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Single<List<Meal>> filterByArea(String area) {
+        AppLogger.d("MealRepository: filterByArea → " + area);
+        return remoteDataSource.filterByArea(area)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Single<List<Meal>> filterByIngredient(String ingredient) {
+        AppLogger.d("MealRepository: filterByIngredient → " + ingredient);
+        return remoteDataSource.filterByIngredient(ingredient)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Single<List<Meal>> listAllAreas() {
+        AppLogger.d("MealRepository: listAllAreas");
+        return remoteDataSource.listAllAreas()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+
+    @Override
+    public Single<List<Meal>> listAllIngredients() {
+        AppLogger.d("MealRepository: listAllIngredients");
+        return remoteDataSource.listAllIngredients()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+}
